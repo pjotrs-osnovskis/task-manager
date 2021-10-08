213e7e226fca9c96d3673f3b31eebf4330cdd278
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-10-08 20:04:05 +0100

    Update task in to database, chanhe home links to login if not logged in

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 # Get tasks route
 @app.route("/")
 @app.route("/get_tasks")
@@ -98,6 +99,7 @@ def logout():
     # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
+    
     return redirect(url_for("login"))
 
 
@@ -123,6 +125,19 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated")
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
